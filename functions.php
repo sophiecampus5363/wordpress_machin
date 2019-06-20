13f0540ce7330c2e2e99cc24f2eadff82f0efa68
Sophie <sophie.jacquot@le-campus_numerique.fr>
2019-06-20 16:58:11 +0200

    jsq video 8 saison 1

--- functions.php
+++ functions.php
@@ -16,7 +16,9 @@ function lgmac_scripts() {
     LGMAC_VERSION, 'all');
 
     //chargement des scripts
-    wp_enqueue_script( 'lgmac_admin_script', get_template_directory_uri() . '/js/machin.js', array('jquery'), 
+    wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), 
+    LGMAC_VERSION, true);
+    wp_enqueue_script( 'lgmac_admin_script', get_template_directory_uri() . '/js/machin.js', array('jquery', 'bootstrap-js'), 
     LGMAC_VERSION, true);
    
 } // fin function lgmac_scripts
@@ -45,10 +47,18 @@ function lgmac_setup() {
     remove_action('wp_head', 'wp_generator');
 
     //enlève les guillemets à la française
-    //remove_filter('the_content', 'wptexturize');
+    remove_filter('the_content', 'wptexturize');
 
     //support du titre
-    add_theme_support('title-tag');
+    add_theme_support('title-tag'); 
+
+    //Register Custom Navigation Walker
+    require_once('includes/class-wp-bootstrap-navwalker.php');
+
+    //active la gestion des menus
+    register_nav_menus( array('primary' => 'principal', ));
+
+
 
 
 
--- index.php
+++ index.php
@@ -7,8 +7,7 @@
     <meta name="description" content="lorem ipsum gna gna gna" />
 
     <?php wp_head(); ?>
-  
-    
+      
   </head>
 
 
@@ -17,90 +16,76 @@
 
   <header>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="/">Home</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+  <!-- <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Features</a>
-      <a class="nav-item nav-link" href="#">Pricing</a>
-      <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      
+      <a class="nav-item nav-link" href="#">Menu 1 </a>
+      <a class="nav-item nav-link" href="#">Menu 2</a>
+      <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Menu 3</a>  -->
+
+    <?php
+    wp_nav_menu( array(
+        'menu'                => 'top-menu',
+        'theme_location'      => 'primary',
+        'depth'               => 2,
+        'container'           => 'div',
+        'container_class'     => 'collapse navbar-collapse',
+        'container_id'        => 'navbarNavAltMarkup',
+        'menu_class'          => 'nav navbar-nav',
+        'fallback_cb'         => 'wp_bootstrap_navwalker::fallback',
+        'walker'              => new wp_bootstrap_navwalker()
+      )
+    ); ?>
+
+
+
     </div>
   </div>
 </nav>
 
   </header>
 
-<div class="container">
+<div class="container"><!-- container -->
     <div class="jumbotron">
         <h1>coucou c'est nous</h1>
     </div>
 </div> <!-- container -->
 
+
+
     <section>
-        <div class="container">
+      <?php if (have_posts()): ?>
+        <div class="container"><!-- container -->
+            <?php  while(have_posts()): the_post(); ?>
 
         <div class="row m-dw-30">
-                <div class="col-xs-2">
-                    <img src="" alt="" class="img-responsive">
+                <div class="col-2">
+                   <?php the_post_thumbnail('thumbnail'); ?>
                 </div> 
-                <div class="clos-xs-10">
-                        <h1> Titre de l'article </h1> 
-                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias placeat nisi modi 
-                                quibusdam quisquam. Esse, sunt. Iusto tenetur excepturi autem officia. Consequuntur nisi 
-                                sed quidem ad ipsa praesentium nihil veritatis.
-                            </p>  
+                <div class="col-10">
+                        <h1><a href="<?php the_permalink(); ?>"> <?php the_title(); ?></a> </h1> 
+                            <?php the_excerpt(); ?>
+
+                            
                 </div>                
         </div> <!-- row -->
 
-        <div class="row m-dw-30">
-                <div class="col-xs-2">
-                    <img src="" alt="" class="img-responsive">
-                </div> 
-                <div class="clos-xs-10">
-                        <h1> Titre de l'article </h1> 
-                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias placeat nisi modi 
-                                quibusdam quisquam. Esse, sunt. Iusto tenetur excepturi autem officia. Consequuntur nisi 
-                                sed quidem ad ipsa praesentium nihil veritatis.
-                            </p>  
-                </div>                
-        </div> <!-- row -->
-
-        <div class="row m-dw-30">
-                <div class="col-xs-2">
-                    <img src="" alt="" class="img-responsive">
-                </div> 
-                <div class="clos-xs-10">
-                        <h1> Titre de l'article </h1> 
-                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias placeat nisi modi 
-                                quibusdam quisquam. Esse, sunt. Iusto tenetur excepturi autem officia. Consequuntur nisi 
-                                sed quidem ad ipsa praesentium nihil veritatis.
-                            </p>  
-                </div>                
-        </div> <!-- row -->
-
-        <div class="row m-dw-30">
-                <div class="col-xs-2">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/fragile-yes.jpg" alt="" class="img-responsive">
-                </div> 
-                <div class="clos-xs-10">
-                        <h1> Titre de l'article </h1> 
-                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias placeat nisi modi 
-                                quibusdam quisquam. Esse, sunt. Iusto tenetur excepturi autem officia. Consequuntur nisi 
-                                sed quidem ad ipsa praesentium nihil veritatis.
-                            </p>  
-                </div>                
-        </div> <!-- row -->
-    
-</div> <!-- container -->
-
-    </section>    
-
-
-    <div> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus voluptate sapiente animi ad quam. Eius, rerum quasi magni iste rem possimus molestias deserunt amet dignissimos, impedit explicabo quos at numquam?
-    </div>
+        <?php endwhile; ?>
+                 
+    <?php else: ?>       
+          <div class="row">
+              <div class="col-12">
+                <p>pas de résultat</p>
+              </div>
+          </div>
+      </div> <!-- container -->
+    <?php endif; ?>
+  </div> <!-- container -->
+</section>    
 
     <?php wp_footer(); ?>
 
